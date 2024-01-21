98e1a32a920bd82499e16886a2fb06d8165d152c
Brandon Gerber <90160923+brandongerber@users.noreply.github.com>
2024-01-20 20:33:56 -0500

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -2,27 +2,23 @@
  * 1/19/2024 
  * COP3804
  */
-import java.util.Scanner;
 public class Task2 {
 	public static void main(String[] args) {
-		// array to store my favorite books
-		String[] books = new String[3];
-		//assign books to array elements
-		books[0] = "Tuesdays with morrie";
-		books[1] = "Make your bed";
-		books[2] = "The four agreements";
-			
-		Scanner favBook = new Scanner (System.in);
-			System.out.print("Search for favorite book. 0-2 :");
-			
-			int bookInput = favBook.nextInt();
-			if (bookInput >= 0 && bookInput < books.length) {
-				if (bookInput >= 0 && bookInput < books.length) {
-		            System.out.println("One of my favorite books is: " + books[bookInput] + " (Index # " + bookInput + ")");
-		        } else {
-		            System.out.println("Invalid input. Please enter a number between 0 and " + (books.length - 1));
+		        // array to store my favorite books
+		        String[] books = new String[3];
+		        // assign books to array elements
+		        books[0] = "Tuesdays with Morrie by Mitch Albom";
+		        books[1] = "Make your Bed by William H. McRaven";
+		        books[2] = "The Four Agreements by Don Miguel Ruiz";
+
+		        // search for book in array 
+		        int searchBooks = 1;
+
+		        // print favorite book
+		        if (searchBooks >= 0 && searchBooks < books.length) {
+		            System.out.println("One of my favorite books is: " + books[searchBooks] + " (# " + searchBooks + ")");
+		        } else 
+		        	System.out.println("Invalid search.");
 		        }
-			}
-	}
-}
-			
+		    }
+					
